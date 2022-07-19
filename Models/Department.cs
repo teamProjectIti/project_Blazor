0374a1e77e187c85141a18d33424638892b19b2e
unknown <beshoyamen5@gmail.com>
2022-07-19 16:15:52 -0700

    first commit

---
+++ Models/Department.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace projectBlazor.Models
+{
+    public class Department
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Student> Students { get; set; }
+    }
+}
---
+++ Models/SampleData.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace projectBlazor.Models
+{
+    public class SampleData
+    {
+
+        public List<Department> Departments { get; set; } = new List<Department>();
+        public List<Student> Students { get; set; } = new List<Student>();
+
+        public SampleData()
+        {
+            Departments.Add(new Department() { Name = "C#", ID = 1 });
+            Departments.Add(new Department() { Name = "php", ID = 2 });
+            Departments.Add(new Department() { Name = "html", ID = 3 });
+            Departments.Add(new Department() { Name = "css", ID = 4 });
+            Departments.Add(new Department() { Name = "js", ID = 5 });
+            Departments.Add(new Department() { Name = "sql", ID = 6 });
+            Departments.Add(new Department() { Name = "oop", ID = 7 });
+            Students.Add(new Student() { ID = 1, Name = "ehab", Department_ID = 1 });
+            Students.Add(new Student() { ID = 1, Name = "bassem", Department_ID = 2 });
+            Students.Add(new Student() { ID = 1, Name = "amire", Department_ID = 3 });
+            Students.Add(new Student() { ID = 1, Name = "samy", Department_ID = 3 });
+            Students.Add(new Student() { ID = 1, Name = "mona", Department_ID = 2 });
+            Students.Add(new Student() { ID = 1, Name = "saad", Department_ID = 6 });
+            Students.Add(new Student() { ID = 1, Name = "bob", Department_ID = 2 });
+        }
+
+        public List<Department> GetDepartments()
+        {
+            return Departments;
+        }
+        public List<Student> GetStudents()
+        {
+            return Students;
+        }
+    }
+}
